144f16acb97a1f1daf11af634aaf4eee0408961c
Heinz N. Gies <heinz@licenser.net>
2019-04-04 22:32:25 +0200

    Add jemalloc and core afinity to benchmark

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,5 +1,10 @@
+extern crate core_affinity;
+extern crate jemallocator;
 #[macro_use]
 extern crate criterion;
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 use criterion::{BatchSize, Benchmark, Criterion, ParameterizedBenchmark, Throughput};
 use serde_json;
@@ -10,6 +15,9 @@ use std::io::{self, Read, Write};
 macro_rules! bench_file {
     ($name:ident) => {
         fn $name(c: &mut Criterion) {
+            let core_ids = core_affinity::get_core_ids().unwrap();
+            core_affinity::set_for_current(core_ids[0]);
+
             let mut vec = Vec::new();
             File::open(concat!("data/", stringify!($name), ".json"))
                 .unwrap()
