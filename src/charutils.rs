62062f3f5ef7b2fdb069a2f297a813d359cc7f92
Heinz Gies <heinz@licenser.net>
2019-04-13 16:07:04 +0200

    More branches removed

--- src/charutils.rs
+++ src/charutils.rs
@@ -22,12 +22,12 @@ const STRUCTURAL_OR_WHITESPACE: [u32; 256] = [
 
 #[cfg_attr(not(feature = "no-inline"), inline(always))]
 pub fn is_not_structural_or_whitespace(c: u8) -> u32 {
-    return STRUCTURAL_OR_WHITESPACE_NEGATED[c as usize];
+    return unsafe { *STRUCTURAL_OR_WHITESPACE_NEGATED.get_unchecked(c as usize) };
 }
 
 #[cfg_attr(not(feature = "no-inline"), inline(always))]
 pub fn is_structural_or_whitespace(c: u8) -> u32 {
-    return STRUCTURAL_OR_WHITESPACE[c as usize];
+    return unsafe { *STRUCTURAL_OR_WHITESPACE.get_unchecked(c as usize) };
 }
 
 const DIGITTOVAL: [i8; 256] = [
@@ -53,11 +53,13 @@ pub fn hex_to_u32_nocheck(src: &[u8]) -> u32 {
     // all these will sign-extend the chars looked up, placing 1-bits into the high 28 bits of every
     // invalid value. After the shifts, this will *still* result in the outcome that the high 16 bits of any
     // value with any invalid char will be all 1's. We check for this in the caller.
-    let v1: i32 = DIGITTOVAL[src[0] as usize] as i32;
-    let v2: i32 = DIGITTOVAL[src[1] as usize] as i32;
-    let v3: i32 = DIGITTOVAL[src[2] as usize] as i32;
-    let v4: i32 = DIGITTOVAL[src[3] as usize] as i32;
-    (v1 << 12 | v2 << 8 | v3 << 4 | v4) as u32
+    unsafe {
+        let v1: i32 = *DIGITTOVAL.get_unchecked(*src.get_unchecked(0) as usize) as i32;
+        let v2: i32 = *DIGITTOVAL.get_unchecked(*src.get_unchecked(1) as usize) as i32;
+        let v3: i32 = *DIGITTOVAL.get_unchecked(*src.get_unchecked(2) as usize) as i32;
+        let v4: i32 = *DIGITTOVAL.get_unchecked(*src.get_unchecked(3) as usize) as i32;
+        (v1 << 12 | v2 << 8 | v3 << 4 | v4) as u32
+    }
 }
 
 // given a code point cp, writes to c
@@ -74,29 +76,31 @@ pub fn hex_to_u32_nocheck(src: &[u8]) -> u32 {
 //
 #[cfg_attr(not(feature = "no-inline"), inline)]
 pub fn codepoint_to_utf8(cp: u32, c: &mut [u8]) -> usize {
-    if cp <= 0x7F {
-        c[0] = cp as u8;
-        return 1; // ascii
-    }
-    if cp <= 0x7FF {
-        c[0] = ((cp >> 6) + 192) as u8;
-        c[1] = ((cp & 63) + 128) as u8;
-        return 2; // universal plane
-                  //  Surrogates are treated elsewhere...
-                  //} //else if (0xd800 <= cp && cp <= 0xdfff) {
-                  //  return 0; // surrogates // could put assert here
-    } else if cp <= 0xFFFF {
-        c[0] = ((cp >> 12) + 224) as u8;
-        c[1] = (((cp >> 6) & 63) + 128) as u8;
-        c[2] = ((cp & 63) + 128) as u8;
-        return 3;
-    } else if cp <= 0x10FFFF {
-        // if you know you have a valid code point, this is not needed
-        c[0] = ((cp >> 18) + 240) as u8;
-        c[1] = (((cp >> 12) & 63) + 128) as u8;
-        c[2] = (((cp >> 6) & 63) + 128) as u8;
-        c[3] = ((cp & 63) + 128) as u8;
-        return 4;
+    unsafe {
+        if cp <= 0x7F {
+            *c.get_unchecked_mut(0) = cp as u8;
+            return 1; // ascii
+        }
+        if cp <= 0x7FF {
+            *c.get_unchecked_mut(0) = ((cp >> 6) + 192) as u8;
+            *c.get_unchecked_mut(1) = ((cp & 63) + 128) as u8;
+            return 2; // universal plane
+                      //  Surrogates are treated elsewhere...
+                      //} //else if (0xd800 <= cp && cp <= 0xdfff) {
+                      //  return 0; // surrogates // could put assert here
+        } else if cp <= 0xFFFF {
+            *c.get_unchecked_mut(0) = ((cp >> 12) + 224) as u8;
+            *c.get_unchecked_mut(1) = (((cp >> 6) & 63) + 128) as u8;
+            *c.get_unchecked_mut(2) = ((cp & 63) + 128) as u8;
+            return 3;
+        } else if cp <= 0x10FFFF {
+            // if you know you have a valid code point, this is not needed
+            *c.get_unchecked_mut(0) = ((cp >> 18) + 240) as u8;
+            *c.get_unchecked_mut(1) = (((cp >> 12) & 63) + 128) as u8;
+            *c.get_unchecked_mut(2) = (((cp >> 6) & 63) + 128) as u8;
+            *c.get_unchecked_mut(3) = ((cp & 63) + 128) as u8;
+            return 4;
+        }
     }
     // will return 0 when the code point was too large.
     return 0; // bad r
