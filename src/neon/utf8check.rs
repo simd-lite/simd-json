4060b9d088d96f6db3fb9702783cb5b6dabaf331
Heinz Gies <heinz@licenser.net>
2019-08-30 23:48:16 +0200

    vqsubq_u8 fix pt. 2

--- src/neon/utf8check.rs
+++ src/neon/utf8check.rs
@@ -68,15 +68,15 @@ unsafe fn continuation_lengths(high_nibbles: int8x16_t) -> int8x16_t {
 
 #[cfg_attr(not(feature = "no-inline"), inline)]
 unsafe fn carry_continuations(initial_lengths: int8x16_t, previous_carries: int8x16_t) -> int8x16_t {
-        let right1: int8x16_t = vqsubq_s8(
-            push_last_byte_of_a_to_b(previous_carries, initial_lengths),
-            vdupq_n_s8(1),
-        );
+        let right1: int8x16_t = vreinterpretq_s8_u8(vqsubq_u8(
+            vreinterpretq_u8_s8(push_last_byte_of_a_to_b(previous_carries, initial_lengths)),
+            vdupq_n_u8(1),
+        ));
         let sum: int8x16_t = vaddq_s8(initial_lengths, right1);
-        let right2: int8x16_t = vqsubq_s8(
-            push_last_2bytes_of_a_to_b(previous_carries, sum),
-            vdupq_n_s8(2),
-        );
+        let right2: int8x16_t = vreinterpretq_s8_u8(vqsubq_u8(
+            vreinterpretq_u8_s8(push_last_2bytes_of_a_to_b(previous_carries, sum)),
+            vdupq_n_u8(2),
+        ));
         vaddq_s8(sum, right2)
 }
 
