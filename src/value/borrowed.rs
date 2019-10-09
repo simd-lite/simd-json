385cc3f814e270499bf5c0cd52baa239d65b51ba
Heinz N. Gies <heinz@licenser.net>
2019-10-09 10:52:24 +0200

    Add function to turn a borrowed value into 'static

--- src/value/borrowed.rs
+++ src/value/borrowed.rs
@@ -46,6 +46,27 @@ pub enum Value<'v> {
     Object(Object<'v>),
 }
 
+impl<'v> Value<'v> {
+    /// Enforces static lifetime on a borrowed value, this will
+    /// force all strings to become owned COW's, the same applies for
+    /// Object keys.
+    pub fn into_static(self) -> Value<'static> {
+        unsafe {
+            use std::mem::transmute;
+            transmute(match self {
+                Self::String(Cow::Borrowed(s)) => Self::String(Cow::Owned(s.to_owned())),
+                Self::Array(arr) => Self::Array(arr.into_iter().map(Value::into_static).collect()),
+                Self::Object(obj) => Self::Object(
+                    obj.into_iter()
+                        .map(|(k, v)| (Cow::Owned(k.into_owned()), v.into_static()))
+                        .collect(),
+                ),
+                _ => self,
+            })
+        }
+    }
+}
+
 impl<'v> ValueTrait for Value<'v> {
     type Object = Object<'v>;
     type Array = Vec<Value<'v>>;
@@ -624,9 +645,12 @@ mod test {
         fn prop_to_owned(borrowed in arb_value()) {
             use crate::OwnedValue;
             let owned: OwnedValue = borrowed.clone().into();
-            dbg!(&owned);
-            dbg!(&borrowed);
             assert_eq!(borrowed, owned);
+        }
+        #[test]
+        fn prop_to_static(borrowed in arb_value()) {
+            let static_borrowed = borrowed.clone().into_static();
+            assert_eq!(borrowed, static_borrowed);
         }
         #[test]
         fn prop_serialize_deserialize(borrowed in arb_value()) {
