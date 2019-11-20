80b05150a9c4653ff7e9a64f89b6e99bb3968e0d
Heinz N. Gies <heinz@licenser.net>
2019-11-20 21:31:45 +0100

    Obey clippy

--- src/sse42/generator.rs
+++ src/sse42/generator.rs
@@ -7,7 +7,7 @@ use crate::value::generator::ESCAPED;
 use std::io;
 
 #[inline(always)]
-pub unsafe fn write_str_simd<W>(
+pub(crate) unsafe fn write_str_simd<W>(
     writer: &mut W,
     string: &mut &[u8],
     len: &mut usize,
