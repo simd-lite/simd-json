4dbff22e74116651a22484fe95700f40cf7ad1ec
Heinz N. Gies <heinz@licenser.net>
2019-12-01 18:15:46 +0100

    Treat alloc tests for different architectors different

--- tests/alloc.rs
+++ tests/alloc.rs
@@ -26,6 +26,14 @@ test!(canada, 4, 0, 3);
 test!(citm_catalog, 4, 0, 3);
 test!(log, 4, 0, 3);
 test!(marine_ik, 4, 1, 3);
-test!(numbers, 4, 0, 3);
 test!(twitter, 4, 0, 3);
 test!(twitterescaped, 4, 0, 3);
+
+// Allocations for this are different on archetecture
+#[cfg(all(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    not(target_feature = "avx2")
+))]
+test!(numbers, 5, 0, 4);
+#[cfg(target_feature = "avx2")]
+test!(numbers, 4, 0, 3);
