476ed98b896b6594d59914a7e661bb2f4569cbcc
Heinz N. Gies <heinz@licenser.net>
2020-01-14 19:34:06 +0100

    128 bit sede tests

---
+++ tests/serde_128_bit.rs
@@ -0,0 +1,60 @@
+
+#[cfg(all(feature = "serde_impl", feature = "128bit"))]
+#[test]
+#[ignore] // https://github.com/serde-rs/serde/issues/1717
+fn lgostash_int_bug() {
+    use simd_json::serde::from_slice;
+    use serde::Deserialize;
+    #[serde(untagged)]
+    #[derive(Deserialize, Debug, PartialEq)]
+    pub enum RawMessageMetricValue {
+        Boolean(bool),
+        Float(f64),
+        Long(i128),
+        Integer(i64),
+        String(String),
+    }
+    #[derive(Deserialize, Debug, PartialEq)]
+    pub struct Message {
+        pub name: String,
+        pub value: RawMessageMetricValue,
+    }
+
+    let mut d = String::from(
+        r#"
+{
+"name": "max_unsafe_auto_id_timestamp",
+"value": -9223372036854776000
+}
+"#,
+    );
+    let mut d = unsafe { d.as_bytes_mut() };
+    let v_serde: Message = serde_json::from_slice(d).expect("serde_json");
+    let v_simd: Message = from_slice(&mut d).expect("simd_json");
+    assert_eq!(v_simd, v_serde)
+}
+
+#[cfg(all(feature = "serde_impl", feature = "128bit"))]
+#[test]
+fn lgostash_int_bug2() {
+    use simd_json::serde::from_slice;
+    use serde::Deserialize;
+    #[derive(Deserialize, Debug, PartialEq)]
+    pub struct Message {
+        pub name: String,
+        pub value: i128,
+    }
+
+    let mut d = String::from(
+        r#"
+{
+"name": "max_unsafe_auto_id_timestamp",
+"value": -9223372036854776000
+}
+"#,
+    );
+    let mut d = unsafe { d.as_bytes_mut() };
+    let v_serde: Message = serde_json::from_slice(d).expect("serde_json");
+    let v_simd: Message = from_slice(&mut d).expect("simd_json");
+    assert_eq!(v_simd, v_serde)
+}
