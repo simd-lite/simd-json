f2f915cba04c24e57daf3dbf697b422d91732da7
Heinz N. Gies <heinz@licenser.net>
2020-04-03 00:19:24 +0200

    inline

--- src/serde/se.rs
+++ src/serde/se.rs
@@ -29,6 +29,7 @@ macro_rules! iomap {
 }
 
 /// Write a value to a vector
+#[inline]
 pub fn to_vec<T>(to: &T) -> crate::Result<Vec<u8>>
 where
     T: ser::Serialize,
@@ -40,6 +41,7 @@ where
 }
 
 /// Write a value to a string
+#[inline]
 pub fn to_string<T>(to: &T) -> crate::Result<String>
 where
     T: ser::Serialize,
@@ -48,6 +50,7 @@ where
 }
 
 /// Write a value to a string
+#[inline]
 pub fn to_writer<T, W>(writer: W, to: &T) -> crate::Result<()>
 where
     T: ser::Serialize,
@@ -63,9 +66,11 @@ where
     W: Write,
 {
     type T = W;
+    #[inline]
     fn get_writer(&mut self) -> &mut Self::T {
         &mut self.0
     }
+    #[inline]
     fn write_min(&mut self, _slice: &[u8], min: u8) -> std::io::Result<()> {
         self.0.write_all(&[min])
     }
@@ -80,6 +85,7 @@ where
 {
     type Ok = ();
     type Error = Error;
+    #[inline]
     fn serialize_element<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
     where
         T: serde_ext::Serialize,
@@ -95,6 +101,7 @@ where
         }
         value.serialize(&mut **s)
     }
+    #[inline]
     fn end(self) -> Result<Self::Ok, Self::Error> {
         iomap!(self.s.write(b"]"))
     }
@@ -106,6 +113,7 @@ where
 {
     type Ok = ();
     type Error = Error;
+    #[inline]
     fn serialize_element<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
     where
         T: serde_ext::Serialize,
@@ -121,6 +129,7 @@ where
         }
         value.serialize(&mut **s)
     }
+    #[inline]
     fn end(self) -> Result<Self::Ok, Self::Error> {
         iomap!(self.s.write(b"]"))
     }
@@ -132,6 +141,7 @@ where
 {
     type Ok = ();
     type Error = Error;
+    #[inline]
     fn serialize_field<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
     where
         T: serde_ext::Serialize,
@@ -147,6 +157,7 @@ where
         }
         value.serialize(&mut **s)
     }
+    #[inline]
     fn end(self) -> Result<Self::Ok, Self::Error> {
         iomap!(self.s.write(b"]"))
     }
@@ -158,6 +169,7 @@ where
 {
     type Ok = ();
     type Error = Error;
+    #[inline]
     fn serialize_field<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
     where
         T: serde_ext::Serialize,
@@ -173,6 +185,7 @@ where
         }
         value.serialize(&mut **s)
     }
+    #[inline]
     fn end(self) -> Result<Self::Ok, Self::Error> {
         iomap!(self.s.write(b"}"))
     }
@@ -189,6 +202,7 @@ where
 {
     type Ok = ();
     type Error = Error;
+    #[inline]
     fn serialize_key<T: ?Sized>(&mut self, key: &T) -> Result<(), Self::Error>
     where
         T: serde_ext::Serialize,
@@ -205,6 +219,7 @@ where
         qtry!(key.serialize(&mut **s));
         iomap!(s.write(b":"))
     }
+    #[inline]
     fn serialize_value<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
     where
         T: serde_ext::Serialize,
@@ -212,6 +227,7 @@ where
         let SerializeMap { ref mut s, .. } = *self;
         value.serialize(&mut **s)
     }
+    #[inline]
     fn end(self) -> Result<Self::Ok, Self::Error> {
         iomap!(self.s.write(b"}"))
     }
@@ -223,6 +239,7 @@ where
 {
     type Ok = ();
     type Error = Error;
+    #[inline]
     fn serialize_field<T: ?Sized>(
         &mut self,
         key: &'static str,
@@ -244,6 +261,7 @@ where
         iotry!(s.write(b":"));
         value.serialize(&mut **s)
     }
+    #[inline]
     fn end(self) -> Result<Self::Ok, Self::Error> {
         iomap!(self.s.write(b"}"))
     }
@@ -255,6 +273,7 @@ where
 {
     type Ok = ();
     type Error = Error;
+    #[inline]
     fn serialize_field<T: ?Sized>(
         &mut self,
         key: &'static str,
@@ -276,6 +295,7 @@ where
         iotry!(s.write(b":"));
         value.serialize(&mut **s)
     }
+    #[inline]
     fn end(self) -> Result<Self::Ok, Self::Error> {
         iomap!(self.s.write(b"}"))
     }
@@ -294,6 +314,7 @@ where
     type SerializeMap = SerializeMap<'w, W>;
     type SerializeStruct = SerializeMap<'w, W>;
     type SerializeStructVariant = SerializeMap<'w, W>;
+    #[inline]
     fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
         if v {
             iomap!(self.write(b"true"))
@@ -301,53 +322,68 @@ where
             iomap!(self.write(b"false"))
         }
     }
+    #[inline]
     fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_int(v as i64))
     }
+    #[inline]
     fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_int(v as i64))
     }
+    #[inline]
     fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_int(v as i64))
     }
+    #[inline]
     fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_int(v as i64))
     }
+    #[inline]
     fn serialize_i128(self, v: i128) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_int128(v))
     }
+    #[inline]
     fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_uint(v as u64))
     }
+    #[inline]
     fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_uint(v as u64))
     }
+    #[inline]
     fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_uint(v as u64))
     }
+    #[inline]
     fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_uint(v as u64))
     }
+    #[inline]
     fn serialize_u128(self, v: u128) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_uint128(v))
     }
 
+    #[inline]
     fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_float(v as f64))
     }
+    #[inline]
     fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
         self.write_float(v)?;
         Ok(())
     }
+    #[inline]
     fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
         // A char encoded as UTF-8 takes 4 bytes at most.
         // taken from: https://docs.serde.rs/src/serde_json/ser.rs.html#213
         let mut buf = [0; 4];
         iomap!(self.write_string(v.encode_utf8(&mut buf)))
     }
+    #[inline]
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write_string(v))
     }
+    #[inline]
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
         iotry!(self.write(b"["));
         if let Some((first, rest)) = v.split_first() {
@@ -359,21 +395,26 @@ where
         }
         iomap!(self.write(b"]"))
     }
+    #[inline]
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
         self.serialize_unit()
     }
+    #[inline]
     fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: serde_ext::Serialize,
     {
         value.serialize(self)
     }
+    #[inline]
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
         iomap!(self.write(b"null"))
     }
+    #[inline]
     fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
         self.serialize_unit()
     }
+    #[inline]
     fn serialize_unit_variant(
         self,
         _name: &'static str,
@@ -383,6 +424,7 @@ where
         iomap!(self.write_string(variant))
     }
 
+    #[inline]
     fn serialize_newtype_struct<T: ?Sized>(
         self,
         _name: &'static str,
@@ -394,6 +436,7 @@ where
         value.serialize(self)
     }
 
+    #[inline]
     fn serialize_newtype_variant<T: ?Sized>(
         self,
         _name: &'static str,
@@ -410,6 +453,7 @@ where
         qtry!(value.serialize(&mut *self));
         iomap!(self.write(b"}"))
     }
+    #[inline]
     fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
         if len == Some(0) {
             iotry!(self.write(b"["));
@@ -426,10 +470,12 @@ where
         }
     }
 
+    #[inline]
     fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
         self.serialize_seq(Some(len))
     }
 
+    #[inline]
     fn serialize_tuple_struct(
         self,
         _name: &'static str,
@@ -438,6 +484,7 @@ where
         self.serialize_seq(Some(len))
     }
 
+    #[inline]
     fn serialize_tuple_variant(
         self,
         _name: &'static str,
@@ -451,6 +498,7 @@ where
         self.serialize_seq(Some(len))
     }
 
+    #[inline]
     fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
         if len == Some(0) {
             iotry!(self.write(b"{"));
@@ -467,6 +515,7 @@ where
         }
     }
 
+    #[inline]
     fn serialize_struct(
         self,
         _name: &'static str,
@@ -475,6 +524,7 @@ where
         self.serialize_map(Some(len))
     }
 
+    #[inline]
     fn serialize_struct_variant(
         self,
         _name: &'static str,
