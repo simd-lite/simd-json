c0562987c98bc22d262731bad5d5cf9165635a2d
Heinz N. Gies <heinz@licenser.net>
2023-04-24 16:13:57 +0200

    Fix arm number parsing for unalligned integer numbers
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- src/numberparse.rs
+++ src/numberparse.rs
@@ -112,8 +112,7 @@ fn parse_eight_digits_unrolled(chars: &[u8]) -> u32 {
 #[cfg(any(target_feature = "neon", target_feature = "simd128"))]
 #[allow(clippy::cast_ptr_alignment)]
 fn parse_eight_digits_unrolled(chars: &[u8]) -> u32 {
-    let val: u64 = unsafe { *(chars.as_ptr().cast::<u64>()) };
-    //    memcpy(&val, chars, sizeof(u64));
+    let val = unsafe { (chars.as_ptr() as *const u64).read_unaligned() }; //    memcpy(&val, chars, sizeof(u64));
     let val = (val & 0x0F0F_0F0F_0F0F_0F0F).wrapping_mul(2561) >> 8;
     let val = (val & 0x00FF_00FF_00FF_00FF).wrapping_mul(6_553_601) >> 16;
 
