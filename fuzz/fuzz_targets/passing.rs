48bd51589b7a5c40aaba5b1b96bb0a6a56ab2f61
Heinz N. Gies <heinz@licenser.net>
2023-10-23 15:59:25 +0200

    Update fuzz and libs
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- fuzz/fuzz_targets/passing.rs
+++ fuzz/fuzz_targets/passing.rs
@@ -2,8 +2,9 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let mut data1 = data.clone().to_vec();
-    if let Ok(ref jo) = simd_json::to_owned_value(&mut data1) {
+    let mut data1 = data.to_vec();
+    let res = simd_json::to_borrowed_value(&mut data1);
+    if let Ok(ref jo) = res {
         jo.to_string();
     }
 });
