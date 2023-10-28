7462261b1417b343184ca6622cde5de712ddc55e
Heinz N. Gies <heinz@licenser.net>
2023-10-28 23:25:43 +0200

    Better allocation checking
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- tests/alloc.rs
+++ tests/alloc.rs
@@ -6,28 +6,33 @@ use alloc_counter::{count_alloc, AllocCounterSystem};
 static A: AllocCounterSystem = AllocCounterSystem;
 
 macro_rules! test {
-    ($file:ident, $alloc:expr, $realloc:expr, $drop:expr) => {
+    ($file:ident, $alloc:expr, $realloc:expr) => {
         #[cfg(feature = "alloc")]
         #[test]
         fn $file() {
-            use simd_json::to_tape;
+            use simd_json::{to_tape_with_buffers, Buffers};
             use std::fs::File;
             use std::io::Read;
             let mut v1 = Vec::new();
             let f = String::from(concat!("data/", stringify!($file), ".json"));
+            let mut buffers = simd_json::Buffers::default();
+            File::open(&f).unwrap().read_to_end(&mut v1).unwrap();
+            let _ = to_tape_with_buffers(&mut v1, &mut buffers);
+            // we only care about the second run as at this point buffer armortized and we no longer depend
+            // on guessing
             File::open(f).unwrap().read_to_end(&mut v1).unwrap();
-            let (count, _v) = count_alloc(|| to_tape(&mut v1));
-            assert!(count.0 <= $alloc);
-            assert!(count.1 <= $realloc);
-            assert!(count.2 <= $drop);
+            let (count, _v) = count_alloc(|| to_tape_with_buffers(&mut v1, &mut buffers));
+            dbg!(count);
+            assert_eq!(count.0, $alloc);
+            assert_eq!(count.1, $realloc);
         }
     };
 }
 
-test!(canada, 5, 0, 4);
-test!(citm_catalog, 5, 0, 4);
-test!(log, 5, 0, 4);
-test!(marine_ik, 5, 1, 4);
-test!(twitter, 5, 0, 4);
-test!(twitterescaped, 5, 0, 4);
-test!(numbers, 5, 0, 4);
+test!(canada, 3, 2);
+test!(citm_catalog, 3, 2);
+test!(log, 1, 2);
+test!(marine_ik, 3, 2);
+test!(twitter, 1, 2);
+test!(twitterescaped, 1, 2);
+test!(numbers, 3, 2);
