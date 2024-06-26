0351a7b5bb48b6e8040babeb9e9fabb6b956c212
Jonas Fierlings <fnoegip@gmail.com>
2024-06-26 20:52:14 +0200

    Fix nightly `clippy::legacy_numeric_constants` lints

--- src/lib.rs
+++ src/lib.rs
@@ -863,7 +863,7 @@ impl<'de> Deserializer<'de> {
         let len = input.len();
         let simd_safe_len = len + SIMDINPUT_LENGTH;
 
-        if len > std::u32::MAX as usize {
+        if len > u32::MAX as usize {
             return Err(Self::error(ErrorType::InputTooLarge));
         }
 
